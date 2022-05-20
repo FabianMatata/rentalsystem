24dbd837cc4d15620d3a1ddb8671a24ea5dd2264
FabianMatata <mfabian601@gmail.com>
2022-05-21 01:32:35 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "Web Developer", "Graphics Designer"],
+    typeSpeed:200,
+    BackSpeed:60,
+    loop:true
+})
